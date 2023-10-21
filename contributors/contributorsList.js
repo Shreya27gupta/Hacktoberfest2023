b138a3ed0ba8510269822ba1041fd3e760e083b4
Shreya27gupta <99602180+Shreya27gupta@users.noreply.github.com>
2023-10-21 16:59:18 +0530

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2064,4 +2064,9 @@ contributors = [
     fullname: "Auro S.",
     username: "https://github.com/aurocodes",
   },
+    {
+    id: 426,
+    fullname: "Shreya Gupta",
+    username: "https://github.com/Shreya27gupta",
+  },
 ];
